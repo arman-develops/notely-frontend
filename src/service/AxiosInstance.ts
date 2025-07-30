3668c40ad5d2feb6ee5f6c494e3ddbf020aa099c
Amon Victor <amon.victor.develops@gmail.com>
2025-07-30 12:56:46 +0300

    added deployed base url

--- src/service/AxiosInstance.ts
+++ src/service/AxiosInstance.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  // baseURL: "https://notely-backend-p9e5.onrender.com/api",
-  baseURL: "http://localhost:3500/api",
+  baseURL: "https://notely-backend-p9e5.onrender.com/api",
+  // baseURL: "http://localhost:3500/api",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
